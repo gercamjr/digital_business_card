a8928ddc84e9912e221b96b2617747030e690fe3
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2022-01-12 15:55:41 -0600

    lets try this again

---
+++ src/components/Interests.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Interests() {
+  return (
+    <div className="Interests">
+      <h2>Interests</h2>
+      <p>Amateur Mobile Photographer. Astronomy. Hiking. Video Games. Travel. Coffee.</p>
+    </div>
+  );
+}
