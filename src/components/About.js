d35018104680f5075a35cdd824bfb33eef4967bc
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2022-01-12 10:41:52 -0600

    finished my solo project

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="About">
+      <h2>About</h2>
+      <p>I am a full stack developer with a particular interest in making things simple and elegant. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+    </div>
+  );
+}
---
+++ src/components/Info.js
@@ -0,0 +1,17 @@
+import React from "react";
+import portrait from  "../images/my portrait.webp";
+
+export default function Info() {
+  return (
+    <div className="Info">
+      <img className="info__image" src={portrait} alt="portrait of Gerardo" />
+      <h1 className="info__name">Gerardo Camorlinga Jr.</h1>
+      <h3>Full Stack Web Developer</h3>
+      <p className="info__website"><a href="https://www.geracomdev.com" target="_blank">geracomdev.com</a></p>
+      <div className="call_to_action_btns">
+          <a href="mailto:gercamjr.dev@gmail.com" target="_blank" className="btn btn_email"><i class="fas fa-envelope"></i> Email</a>
+          <a href="https://www.linkedin.com/in/gera-camo-jr/" target="_blank" className="btn btn_linkedin"><i class="fab fa-linkedin"></i> LinkedIn</a>
+      </div>
+    </div>
+  );
+}
