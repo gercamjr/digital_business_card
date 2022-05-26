5b4d59f8d8d5650b5937e62631243c5e33dbe7f2
gercamjr <gercamjr.dev@gmail.com>
2022-05-26 17:40:53 -0500

    updated website info

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className="About">
       <h2>About</h2>
-      <p>I am a full stack developer with a particular interest in making things simple and elegant. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+      <p>I am a Full Stack Web Developer with a particular interest in making things simple and elegant. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
     </div>
   );
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -7,7 +7,7 @@ export default function Info() {
       <img className="info__image" src={portrait} alt="portrait of Gerardo" />
       <h1 className="info__name">Gerardo Camorlinga Jr.</h1>
       <h3>Full Stack Web Developer</h3>
-      <p className="info__website"><a href="https://www.geracomdev.com" target="_blank">geracomdev.com</a></p>
+      <p className="info__website"><a href="https://www.geracomdev.com" target="_blank">www.geracomdev.com</a></p>
       <div className="call_to_action_btns">
           <a href="mailto:gercamjr.dev@gmail.com" target="_blank" className="btn btn_email"><i class="fas fa-envelope"></i> Email</a>
           <a href="https://www.linkedin.com/in/gera-camo-jr/" target="_blank" className="btn btn_linkedin"><i class="fab fa-linkedin"></i> LinkedIn</a>
